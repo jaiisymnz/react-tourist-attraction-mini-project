fc286a8c32be0d1683813d5530a393e31e224321
jaiisymnz <t.yurapon@gmail.com>
2024-12-11 11:39:24 +0700

    fix: vite deployment confilct

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // ค่าเริ่มต้นคือ 'dist'
+  },
+  server: {
+    port: 3000,
+  },
+  base: "./", // เพิ่ม base path เพื่อให้ Asset paths ถูกต้อง
+});
